5a381dfeb085832040bf00dba37b9e4cb4a74b2b
Augusto Amaral Pereira <guto_wow@hotmail.com>
2022-12-12 19:49:28 -0300

    Agora sim fiz a coisa certa

--- src/client.js
+++ src/client.js
@@ -1,64 +1,66 @@
-const { makeAQuestion, sendMessage } = require("./utils");
+const { oFamosoScanf, sendMessage, cadastrarIp, usuariosComMensagens, formatarMensagem, limparTela, mostrarOpcoes } = require("./utils");
 
 /**
  *
- * @param {Array} messages
- * @param {Array} listaDeIps
+ * @param {Array<{ip: string; nome: string; porta: string; chave: string; mensagem: string}>} messages
+ * @param {Array<{ip: string; nome: string; porta: number}>} listaDeIps
+ * @param {{ip: string; nome: string; porta: number}} meusDados
  * @returns
  */
-module.exports = async (messages, listaDeIps) => {
-  console.log("\n\n");
-  console.log("--Menu--");
-  console.log("Digite uma das opções");
-  console.log("1 - Listar IPs");
-  console.log("2 - Adicionar IP");
-  console.log("3 - Remover IP");
-  console.log("4 - Listar mensagens");
-  console.log("5 - Enviar mensagem");
-  console.log("6 - Sair");
+module.exports = async (messages, listaDeIps, meusDados) => {
 
   return new Promise(async (r) => {
     var finish = false;
-    const selectedOpt = await makeAQuestion("Digite uma das opções: ");
-    console.log("\n");
+    mostrarOpcoes()
+    const selectedOpt = await oFamosoScanf("Digite uma das opções: ");
+    limparTela()
 
     switch (selectedOpt) {
       case "1":
-        console.log("Lista de IPs: ");
-        console.log(
-          listaDeIps.length > 0 ? listaDeIps.join(", ") : "Lista vazia"
-        );
+        if (listaDeIps.length > 0) {
+          console.log("Lista de Usuários: ");
+          console.table(listaDeIps)
+        } else console.log('Lista Vazia');
         break;
       case "2":
-        await makeAQuestion(
+        await oFamosoScanf(
           "Digite o IP (Com a porta no final Ex. 127.0.0.1:3000): "
-        ).then((ip) => {
-          listaDeIps.push(ip);
-          console.log(`IP adicionado: ${ip}`);
+        ).then((ip) => cadastrarIp(ip, meusDados)
+        ).then(dados => {
+          listaDeIps.push(dados);
+          console.log(`Usuário ${dados.nome} adicionado.`)
         });
-
         break;
       case "3":
-        console.log("Opções de IP:\n");
-        console.log(listaDeIps.map((str, k) => `${k} - ${str}`).join("\n"));
-        await makeAQuestion("Digite o IP: ").then((ip) => {
-          listaDeIps.splice(listaDeIps.indexOf(listaDeIps[ip]), 1);
-          console.log(`IP removido: ${ip}`);
+        console.log("Lista de Usuários: ");
+        console.table(listaDeIps)
+        await oFamosoScanf("Digite o índice: ").then((idx) => {
+          console.log(`Usuário ID: ${idx}, nome: ${listaDeIps[idx].nome} Removido com sucesso!`);
+          listaDeIps.splice(idx, 1);
         });
         break;
       case "4":
-        console.log(
-          messages.length > 0 ? messages.join("\n") : "Não há mensagens"
-        );
+        if (messages.length > 0) {
+          console.table(usuariosComMensagens(messages))
+          const usuario = await oFamosoScanf("Digite o nome do usuário ou 0 para todos usuários: ");
+          if (usuario === '0') {
+            console.table(formatarMensagem(messages))
+          } else {
+            const mensagensParaExibir = messages.filter((msg) => msg.nome === usuario)
+            if (mensagensParaExibir.length > 0)
+              console.table(formatarMensagem(mensagensParaExibir))
+            else console.log("Não há mensagens");
+          }
+        } else console.log("Não há mensagens");
         break;
       case "5":
-        const messageToSend = await makeAQuestion("Digite a mensagem: ");
+        const messageToSend = await oFamosoScanf("Digite a mensagem: ");
 
         console.log("Opções de IP:\n");
-        console.log(listaDeIps.map((str, k) => `${k} - ${str}`).join("\n"));
-        const ipToSend = await makeAQuestion("Selecione o ID do IP na lista: ");
+        console.table(listaDeIps)
+        const ipToSend = await oFamosoScanf("\n Digite o ID do IP na lista: ");
 
-        await sendMessage(messageToSend, listaDeIps[ipToSend]);
+        await sendMessage(messageToSend, meusDados, listaDeIps[ipToSend]);
 
         break;
       case "6":
--- src/server.js
+++ src/server.js
@@ -1,52 +1,63 @@
 const express = require("express");
 const client = require("./client");
+const { novaListaDeIps, novaListaDeMensagens, limparTela, descriptografar, checarChaveCadastrada } = require("./utils");
 
 
 //Express é o framework de javascript que usamos para escutar as mensagens.
 const app = express();
-const port = 3000;
-
-//Lista contendo todas as mensagens
-var messages = [];
-//Lista contendo os IPs dos usuários
-var listaDeIps = [
-  "127.0.0.1:3000",
-  "52.190.255.41:3000", // Guto
-  "20.195.194.147:3000", // Matheus
-  "20.197.232.133:3000", // leonardo
-  "20.206.66.31:3000", // bruno
-];
+const porta = 3000;
 
 //Configuramos o express para interpretar a mensagem no corpo de uma requisição
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
-//E ativamos o proxy para gravar o IP de quem enviou
-app.set("trust proxy", true);
 
-//Aqui a gente coloca a aplicação para escutar na rota '/'
-app.post("/", (req, res) => {
-  //Aqui nós adicionamos o ip e a mensagem na lista de mensagem
-  messages.push(`IP: ${req.ip} - Mensagem: ${req.body}`);
-  //E exibimos para o usuário que há uma nova mensagem
-  console.log("Nova mensagem recebida!");
-  //E respodemos quem enviou a mensagem que a mensagem foi recebida
-  res.send("Ok");
+
+var meusDados = {
+  ip: '127.0.0.1',
+  nome: 'Jhonatan',
+  porta: `${porta}`,
+  chave: 'chaveUltraSecreta'
+}
+
+var listaDeIps = novaListaDeIps()
+var messages = novaListaDeMensagens();
+
+app.post("/mensagem", (req, res) => {
+  const { chave } = req.headers
+  const destinatario = checarChaveCadastrada(listaDeIps, chave)
+  const { ip, nome, porta } = destinatario
+  if (destinatario) {
+    messages.push({ ip, nome, porta, mensagem: descriptografar(req.body, destinatario, meusDados), mensagemCriptografada: req.body });
+    console.log("Nova mensagem recebida!");
+    res.send("Ok");
+  } else {
+    res.status(500).send('Usuário não cadastrado')
+  }
+});
+
+app.post("/cadastro", (req, res) => {
+  const { ip, nome, porta, chave } = req.headers
+  listaDeIps.push({ ip, nome: nome, porta, chave })
+  console.log(`Usuário: ${nome} cadastrado!`);
+  res.send(meusDados);
 });
 
 
 const main = async () => {
   //Aqui abrimos o servidor para escutar na porta 3000 e
   //Assim que o servidor passa a escutar na porta 3000
-  const server = app.listen(port, async () => {
-    console.log(`Server open http://localhost:${port}`);
+  const server = app.listen(porta, async () => {
+    console.log(`Servidor aberto http://localhost:${porta}`);
   });
+
   //Aguardamos 1 segundo para mostrar o menu
-  await new Promise((r) => setTimeout(r, 1000));
+  await new Promise((r) => setTimeout(r(), 1000));
 
   var rodando = true;
+  limparTela()
   while (rodando) {
     //Aqui usamos uma função para executar as funções do programa
-    rodando = await client(messages, listaDeIps);
+    rodando = await client(messages, listaDeIps, meusDados);
   }
 
   server.close();
